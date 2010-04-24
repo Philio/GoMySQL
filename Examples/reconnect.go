12498b92858d4cdf3b91b57cb503432e833daaf0
Phil Bayfield <phil@bayfmail.com>
2010-04-24 12:36:19 +0100

    slightly ammended reconnect example

--- Examples/reconnect.go
+++ Examples/reconnect.go
@@ -14,8 +14,11 @@ import (
 func reconnect(db *mysql.MySQL, done chan int) {
 	attempts := 0
 	for {
+		// Sleep for 1 second
 		time.Sleep(1000000000)
+		// Attempt to reconnect
 		db.Reconnect()
+		// If there was no error break for loop
 		if db.Errno == 0 {
 			break
 		} else {
@@ -45,7 +48,7 @@ func main() {
 			fmt.Printf("Error #%d %s\n", db.Errno, db.Error)
 			done := make(chan int)
     			go reconnect(db, done)
-    			<-done
+    			<- done
 		}
 		// Sleep for 0.5 seconds
 		time.Sleep(500000000)
