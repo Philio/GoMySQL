bd3d5951667c98f58907f30a0785cead2c0549b4
Phil Bayfield <phil@bayfmail.com>
2011-02-09 01:00:59 +0000

    error updates for prev commit

--- error.go
+++ error.go
@@ -5,6 +5,9 @@
 // license that can be found in the LICENSE file.
 package mysql
 
+import "fmt"
+
+// Client error codes
 type Errno int
 
 const (
@@ -21,8 +24,11 @@ const (
 	CR_MALFORMED_PACKET     Errno = 2027
 	CR_NO_PREPARE_STMT      Errno = 2030
 	CR_PARAMS_NOT_BOUND     Errno = 2031
+	CR_DATA_TRUNCATED       Errno = 2032
+	CR_ALREADY_CONNECTED    Errno = 2058
 )
 
+// Client error descriptions
 type Error string
 
 const (
@@ -39,4 +45,28 @@ const (
 	CR_MALFORMED_PACKET_STR     Error = "Malformed Packet"
 	CR_NO_PREPARE_STMT_STR      Error = "Statement not prepared"
 	CR_PARAMS_NOT_BOUND_STR     Error = "No data supplied for parameters in prepared statement"
+	CR_DATA_TRUNCATED_STR       Error = "Data truncated"
+	CR_ALREADY_CONNECTED_STR    Error = "This handle is already connected"
 )
+
+// Client error struct
+type ClientError struct {
+	Errno Errno
+	Error Error
+}
+
+// Convert to string
+func (e *ClientError) String() string {
+	return fmt.Sprintf("#%d %s", e.Errno, e.Error)
+}
+
+// Server error struct
+type ServerError struct {
+	Errno Errno
+	Error Error
+}
+
+// Convert to string
+func (e *ServerError) String() string {
+	return fmt.Sprintf("#%d %s", e.Errno, e.Error)
+}
